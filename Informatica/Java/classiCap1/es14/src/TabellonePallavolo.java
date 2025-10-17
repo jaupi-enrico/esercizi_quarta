a742724cd6df2e299d627906b9097f419ba61f94
jaupi-enrico <78313537@itiscastelli.onmicrosoft.com>
2025-10-17 09:41:43 +0200

    Add initial volleyball scoreboard project files
    Created the es14 project structure with IntelliJ IDEA configuration files, .gitignore files, and initial Java source files Main.java and TabellonePallavolo.java. The TabellonePallavolo class defines basic fields for a volleyball scoreboard.

---
+++ Informatica/Java/classiCap1/es14/src/TabellonePallavolo.java
@@ -0,0 +1,12 @@
+public class TabellonePallavolo {
+    private String nomeLocale;
+    private String nomeOspite;
+    private int puntiLocale;
+    private int puntiOspite;
+    private int setLocale;
+    private int setOspite;
+    private boolean serveLocale;
+    private boolean serveOspite;
+
+
+}
