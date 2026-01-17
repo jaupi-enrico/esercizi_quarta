78b9bd2aaba1888ece7ca1580703d8e9b78caea9
jaupi-enrico <78313537@itiscastelli.onmicrosoft.com>
2026-01-17 09:44:10 +0100

    Implement basic calculator UI and logic in JavaFX
    Added input fields for two numbers, operation buttons, and result display to the calculator UI. Updated HelloController to perform arithmetic operations (+, -, *, /) based on user input and display the result. The UI now shows the selected operation and updates the result dynamically.

--- Informatica/Java/JavaFX/operazioni/src/main/java/com/calcolatrice/operazioni/HelloController.java
+++ Informatica/Java/JavaFX/operazioni/src/main/java/com/calcolatrice/operazioni/HelloController.java
@@ -2,41 +2,62 @@ package com.calcolatrice.operazioni;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    private Label resultLabel;
+    @FXML
+    private Label signLabel;
+    @FXML
+    private TextField numberOneTextField;
+    @FXML
+    private TextField numberTwoTextField;
 
     String operazione = "";
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
-
-    @FXML
     protected void onPlusButtonClick() {
         operazione = "+";
+        signLabel.setText("+");
+        eseguis();
     }
 
     @FXML
     protected void onMinusButtonClick() {
         operazione = "-";
+        signLabel.setText("-");
+        eseguis();
     }
 
     @FXML
     protected void onPersButtonClick() {
         operazione = "*";
+        signLabel.setText("*");
+        eseguis();
     }
 
     @FXML
     protected void onDivisButtonClick() {
         operazione = "/";
+        signLabel.setText("/");
+        eseguis();
     }
 
     public void eseguis() {
+        double num1 = Double.parseDouble(numberOneTextField.getText());
+        double num2 = Double.parseDouble(numberTwoTextField.getText());
         if (operazione.equals("+")) {
-            welcomeText.setText("4");
+            resultLabel.setText(Double.toString(num1 + num2));
+        }
+        else if (operazione.equals("-")) {
+            resultLabel.setText(Double.toString(num1 - num2));
+        }
+        else if (operazione.equals("*")) {
+            resultLabel.setText(Double.toString(num1 * num2));
+        }
+        else if (operazione.equals("/")) {
+            resultLabel.setText(Double.toString(num1 / num2));
         }
     }
 }
